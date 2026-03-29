87c9ac7fbd11fcd16cd2cad18342ba810a45dbb6
thavaneshmuthuraja <thavanesh9788@gmail.com>
2026-03-30 00:26:01 +0530

    Time: 2 ms (37.43%), Space: 45.7 MB (23.81%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,24 +1,38 @@
 class Solution {
-    private void recurpre(int[] nums,List<List<Integer>> ans,List<Integer> ds,boolean freq[]){
-        if(ds.size() == nums.length){
-            ans.add(new ArrayList<>(ds));
-            return;
+    
+    List<List<Integer>>res=new ArrayList<>();
+
+    void func(int []nums,Map<Integer,Boolean>vis,List<Integer>temp,int n)
+    {
+        if(temp.size()==n)
+        {
+            res.add(new ArrayList<>(temp));
+
         }
-        for(int i = 0;i < nums.length;i++){
-            if(!freq[i]){
-                freq[i] = true;
-                ds.add(nums[i]);
-                recurpre(nums,ans,ds,freq);
-                ds.remove(ds.size()-1);
-                freq[i] = false;
+
+        for(int i=0;i<n;i++)
+        {
+            if(!vis.getOrDefault(nums[i],false))
+            {
+                vis.put(nums[i],true);
+
+                temp.add(nums[i]);
+
+                func(nums,vis,temp,n);
+
+                temp.remove(temp.size()-1);
+
+                vis.put(nums[i],false);
             }
         }
     }
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> ds = new ArrayList<>();
-        boolean freq[] = new boolean[nums.length];
-        recurpre(nums,ans,ds,freq);
-        return ans;
+
+        Map<Integer,Boolean>vis=new HashMap<>();
+
+        func(nums,vis,new ArrayList<Integer>(),nums.length);
+
+        return res;
+        
     }
 }
