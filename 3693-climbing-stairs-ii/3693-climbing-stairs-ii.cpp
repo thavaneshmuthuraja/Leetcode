107f103244816229d4181dc690f0d4fb6cc6e4e9
thavaneshmuthuraja <thavanesh9788@gmail.com>
2026-06-09 23:10:11 +0530

    Time: 13 ms (68.19%), Space: 177.7 MB (75.5%) - LeetHub

--- 3693-climbing-stairs-ii/3693-climbing-stairs-ii.cpp
+++ 3693-climbing-stairs-ii/3693-climbing-stairs-ii.cpp
@@ -32,9 +32,27 @@ public:
 
     int climbStairs(int n, vector<int>& costs) {
 
-        vector<int> mem(n,-1);
+        vector<int> mem(n+1,-1);
 
-       return  func(n,costs,0,mem);
-        
+        mem[0]=0;
+
+        for(int i=1;i<=n;i++)
+        {
+            int ans=1e9;
+        if(i-1>=0) {
+
+            ans=min(ans,costs[i-1]+1+mem[i-1]);
+        }
+        if(i-2>=0) {
+
+            ans=min(ans,costs[i-1]+4+mem[i-2]);
+        }
+        if(i-3>=0) {
+
+            ans=min(ans,costs[i-1]+9+mem[i-3]);
+        }
+        mem[i]=ans;
+        }
+        return mem[n];
     }
 };
