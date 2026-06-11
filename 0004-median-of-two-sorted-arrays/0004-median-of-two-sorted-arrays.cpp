96887cab7dfb2d3caa75755654b0bb6c42ae2ef0
thavaneshmuthuraja <thavanesh9788@gmail.com>
2026-06-11 12:11:27 +0530

    Time: 2 ms (31.68%), Space: 94.9 MB (94.35%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,45 +1,70 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>merge;
+
         int s1=0,s2=0,n1=nums1.size(),n2=nums2.size();
+        int len=n1+n2,in=0,c_in=0,ans=0;
+
+        c_in=len/2;
+        cout<<c_in;
+
+        int in_val=-1;
+
         while(s1<n1 && s2<n2)
         {
+
             if(nums1[s1]<nums2[s2])
             {
-                merge.push_back(nums1[s1]);
-                s1++;
+                in_val=nums1[s1++];
             }else if(nums1[s1]>nums2[s2])
             {
-                merge.push_back(nums2[s2]);
-                s2++;
+                in_val=nums2[s2++];
             }
             else
             {
-                merge.push_back(nums1[s1]);
-                merge.push_back(nums1[s1]);
+                in_val=nums1[s1];
                 s1++;
-                s2++;
             }
+            if(len%2==0 && (in==c_in || in==c_in-1) )
+            {
+                ans+=in_val;
+            }
+            if(len%2==1 && c_in==in)
+            {
+                return (double)in_val;
+            }
+            in++;
         }
 
         while(s1<n1)
         {
-            merge.push_back(nums1[s1++]);
+            in_val=nums1[s1++];
+
+            if(len%2==0 && (in==c_in || in==c_in-1) )
+            {
+                ans+=in_val;
+            }
+            if(len%2==1 && c_in==in)
+            {
+                return (double)in_val;
+            }
+            in++;
+            
         }
         while(s2<n2)
         {
-            merge.push_back(nums2[s2++]);
+            in_val=nums2[s2++];
+            if(len%2==0 && (in==c_in || in==c_in-1) )
+            {
+                ans+=in_val;
+            }
+            if(len%2==1 && c_in==in)
+            {
+                return (double)in_val;
+            }
+            in++;
         }
-        int len=merge.size();
-        cout<<len;
-        
-        if(len%2==0)
-        {
-            return double((merge[(len/2)]+merge[(len/2)-1]))/2;
-        }else 
-        {
-            return (double)merge[(len/2)];
-        }
+
+        return (double)ans/2;
     }
 };
