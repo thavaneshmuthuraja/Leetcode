96716ffb4f18c77050d3761f2c4fa5720d11de25
thavaneshmuthuraja <thavanesh9788@gmail.com>
2026-06-13 10:31:07 +0530

    Time: 2 ms (96.85%), Space: 46.5 MB (65.52%) - LeetHub

--- 3838-weighted-word-mapping/3838-weighted-word-mapping.java
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.java
@@ -6,9 +6,9 @@ class Solution {
         for(String str: words)
         {
             int sum=0;
-            for(char ch:str.toCharArray())
+            for(int i=0;i<str.length();i++)
             {
-                 sum+=weights[ch-'a'];
+                 sum+=weights[str.charAt(i)-'a'];
             }
             sum=sum%26;
             ans.append((char)(25-sum+'a'));
