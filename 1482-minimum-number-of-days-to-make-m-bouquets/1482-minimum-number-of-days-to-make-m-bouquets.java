39074eb656651639f8c478bab9867a3fca038fe7
thavaneshmuthuraja <thavanesh9788@gmail.com>
2026-06-19 23:42:22 +0530

    Time: 16 ms (91.88%), Space: 82.7 MB (45.31%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -1,44 +1,50 @@
 class Solution {
+    public boolean canMake (int[] bloomDay, int m, int k , int n )
+    {   int ans = 0 ; int count = 0 ;
+        for( int i =0 ; i  < bloomDay.length ; i++){
+           if( bloomDay[i] <= n ){
+            count++;
+           }else {
+            ans+= count/k;
+            count = 0;
+           }
 
-    public boolean check(int[]bloom,int m,int k,int day)
-    {
-        int cnt=0;
-        for(int v:bloom)
-        {
-            if(v<=day)
-            {
-                cnt++;
-            }else
-            {
-                cnt=0;
-            }
-            if(cnt==k)
-            {
-                m--;
-                cnt=0;
-            }
-            if(m==0) break;
         }
-        return m<=0 ? true:false;
+
+        ans+= count/k;
+
+        if(ans >= m )return true ;
+        return false ;
+        
     }
     public int minDays(int[] bloomDay, int m, int k) {
-        int l=1,h=(int)1e9;
-        int ans=-1;
+        
 
-        while(l<=h)
-        {
-            int mid=l+(h-l)/2;
+        if( bloomDay.length < m*k)return -1 ;
+       
+       int high = bloomDay[0] ;
+       int low = bloomDay[0] ;
 
-            if(check(bloomDay,m,k,mid))
-            {
-                ans=mid;
-                h=mid-1;
-            }else
-            {
-                l=mid+1;
+       for( int i =0 ; i < bloomDay.length ; i++){
+           low = Math.min(low , bloomDay[i]);
+            high = Math.max(high , bloomDay[i]);
+       }
+
+        int ans = -1 ;
+       while( low <= high ){
+            int mid = low + ( high  - low )/2 ;
+
+            if( canMake(bloomDay , m , k , mid )){
+                ans = mid ;
+                high = mid - 1 ;
+            }else {
+                low = mid + 1 ;
             }
-        }
+       }
 
-        return ans;
+        return ans ;
+
+        
+
     }
 }
