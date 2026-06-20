63b5e3bf81dc299a4a9a5eb230da3aba739e8f9c
thavaneshmuthuraja <thavanesh9788@gmail.com>
2026-06-20 12:05:05 +0530

    Time: 0 ms (100%), Space: 44.2 MB (82.25%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.java
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.java
@@ -4,11 +4,9 @@ class Solution {
         int x=0;
         for(int i=0;i<operations.length;i++)
         {
-        char f=operations[i].charAt(0);
         char s=operations[i].charAt(1);
-        char t=operations[i].charAt(2);
 
-        if(f=='-' ||s=='-')
+        if(s=='-')
         {
             x--;
         }else
