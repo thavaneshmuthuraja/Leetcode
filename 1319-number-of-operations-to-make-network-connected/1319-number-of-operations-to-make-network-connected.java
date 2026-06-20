8a2343dea81eb0beda19f6d50d9457ee273be2b5
thavaneshmuthuraja <thavanesh9788@gmail.com>
2026-06-20 11:42:03 +0530

    Time: 4 ms (92.4%), Space: 64.9 MB (98.29%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -9,25 +9,11 @@ class Solution {
         return par[rep]=find(par,par[rep]);
     }
 
-    public void unite(int []rank,int[]par,int x,int y)
+    public void unite(int[]par,int x,int y)
     {
         int repx=find(par,x);
         int repy=find(par,y);
-
-        if(rank[repx]>rank[repy])
-        {
-            par[repy]=repx;
-
-        }else if(rank[repx]<rank[repy])
-        {
-            par[repx]=repy;
-
-        }else
-        {
-            rank[repx]++;
-            par[repy]=repx;
-        }
-
+        par[repy]=repx;
     }
     public int makeConnected(int n, int[][] connections) {
 
@@ -38,8 +24,6 @@ class Solution {
         }
 
         int []par=new int[n];
-        int []rank=new int[n];
-        Arrays.fill(rank,0);
 
         for(int i=0;i<n;i++)
         {
@@ -50,7 +34,7 @@ class Solution {
         {
             if(find(par,c[0])!=find(par,c[1]))
             {
-                unite(rank,par,c[0],c[1]);
+                unite(par,c[0],c[1]);
             }
         }
         int ncon=0;
